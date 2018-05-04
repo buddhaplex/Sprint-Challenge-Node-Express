93fede4b4b18a3a53c8c85d68ec78f2898b143ad
BUDDHAPLEX <ericandrade@gmail.com>
2018-05-04 11:43:06 -0400

    added actionRoutes.js and projectRoutes.js files (unpopulated)

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 // instantiate server
 const server = express();
 
+server.get('/', function(req, res) {
+  res.json('Api Running...');
+});
+
 // direct to listening port 
 const port = 8000;
 server.listen(port, () => console.log('API is running on port 8000.'));
