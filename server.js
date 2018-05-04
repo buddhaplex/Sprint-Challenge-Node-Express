aa7e6ab46bd7d9eb485c79c4e2db7647cdc164b1
BUDDHAPLEX <ericandrade@gmail.com>
2018-05-04 13:24:05 -0400

    tweaking server.js file, and added code into actionRoutes.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-// require express
+// requirements (express, helmet, cors, morgan)
 const express = require('express');
-
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('cors');
+const actionRouter = require('./routes/actionRoutes.js');
+const projectRouter = require('./routes/projectRoutes.js');
 // instantiate server
 const server = express();
-
-server.get('/', function(req, res) {
-  res.json('Api Running...');
-});
-
 // direct to listening port 
 const port = 8000;
+
+const logger = (req, res) => {
+  console.log(`Requested URL: ${req.url}`);
+  console.log(`Request Info: ${req.body}`);
+}
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(morgan('dev'));
+server.use('/api/actions', actionRoutes);
+server.use('api/projects', projectRoutes);
+
 server.listen(port, () => console.log('API is running on port 8000.'));
-
-
